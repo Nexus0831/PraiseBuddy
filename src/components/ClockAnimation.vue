0fc8f12ccab2cfbad9af749deb27d78c91bb08b2
Nexus0831 <pride0831@gmail.com>
2022-11-07 14:45:57 +0900

    feat: 時間毎の背景を作成

--- src/components/ClockAnimation.vue
+++ src/components/ClockAnimation.vue
@@ -8,11 +8,35 @@
         {{ nowTime }}
       </div>
     </div>
-    <div class="scene morning-background">
+    <div
+      class="scene"
+      :class="[
+        { 'morning-background': 6 <= nowHour && nowHour <= 9 },
+        { 'noon-background': 10 <= nowHour && nowHour <= 16 },
+        { 'sunset-background': 17 <= nowHour && nowHour <= 19 },
+        { 'night-background': nowHour <= 5 || 20 <= nowHour },
+      ]"
+    >
       <div class="mountain-1"></div>
       <div class="mountain-2"></div>
-      <div class="sun-container morning">
-        <div class="sun"></div>
+      <div class="sun-container">
+        <div
+          class="sun"
+          :class="[
+            { morning: 6 <= nowHour && nowHour <= 9 },
+            { noon: 10 <= nowHour && nowHour <= 16},
+            { sunset: 17 <= nowHour && nowHour <= 19},
+          ]"
+          v-if="6 <= nowHour && nowHour <= 19"
+        >
+        </div>
+        <div
+          class="moon"
+          v-if="nowHour <= 5 || 20 <= nowHour"
+        >
+        </div>
+        <div class="cloud one"></div>
+        <div class="cloud two"></div>
       </div>
     </div>
   </div>
@@ -26,6 +50,8 @@ export default class ClockAnimation extends Vue {
   private nowToDay = '';
 
   private nowTime = '';
+
+  private nowHour = 0;
 
   showClock() {
     const d = new Date();
@@ -42,6 +68,7 @@ export default class ClockAnimation extends Vue {
     const day = weekday[dayNum];
     // 時を取得
     let hour: string | number = d.getHours();
+    this.nowHour = hour;
     // 分を取得
     let min: string | number = d.getMinutes();
     // 秒を取得
@@ -92,11 +119,21 @@ export default class ClockAnimation extends Vue {
     .morning-background
       background linear-gradient(#B2EBF2, #FFE082)
 
+    .noon-background
+      background-color #B2EBF2
+
+    .sunset-background
+      background-color #FB8C00
+
+    .night-background
+      background-color #1A237E
+
     .scene
       grid-row 4 / 5
       display grid
       grid-template-columns 50% 50%
       grid-template-rows 50% 50%
+      overflow hidden
 
       .mountain-1
         grid-column 1 / 2
@@ -105,25 +142,82 @@ export default class ClockAnimation extends Vue {
         margin-right -20px
         border-radius 40% 60% 0 0
         z-index 4000
+        box-shadow: 0px 11px 15px -7px rgba(0, 0, 0, 0.2),
+          0px 24px 38px 3px rgba(0, 0, 0, 0.14),
+          0px 9px 46px 8px rgba(0, 0, 0, 0.12);
+
       .mountain-2
         grid-column 2 / 3
         grid-row 1 / 3
         background-color #4CAF50
         border-radius 90% 0 0 0
         z-index 5000
+        box-shadow: 0px 11px 15px -7px rgba(0, 0, 0, 0.2),
+          0px 24px 38px 3px rgba(0, 0, 0, 0.14),
+          0px 9px 46px 8px rgba(0, 0, 0, 0.12);
 
-      .morning
+      .sun-container
+        grid-column 1 / 3
+        grid-row 1 / 2
         display flex
         align-items flex-end
-        filter drop-shadow(0 0 24px #FFFF00)
-      .sun-container
-        grid-column 1 / 2
-        grid-row 1 / 2
 
         .sun
           width 128px
           height 128px
           border-radius 50%
-          background-color #FFEB3B
+          z-index 2000
+          box-shadow 9px 11px 20px -10px rgba(0,0,0,0.6)
+
+        .moon
+          width 100px
+          height 100px
+          border-radius 50%
+          box-shadow -26px 0px 0px 20px #FDD835;
+          transform translate(100px, -100px) rotate(-160deg)
+          filter drop-shadow(0 0 12px #FDD835)
+          z-index 2000
+          /*background-color #FDD835*/
+
+        .morning
+          background-color #FFF59D
+          filter drop-shadow(0 0 24px #FFF59D)
+
+        .noon
+          background-color #FFF176
+          filter drop-shadow(0 0 24px #FFF176)
+          transform translate(150px, -90px)
+
+        .sunset
+          background-color #FDD835
+          filter drop-shadow(0 0 12px #FDD835)
+          transform translate(210px, 50px)
+
+        .cloud
+          width 204px
+          height 64px
+          background-color #ffffff
+          border-radius 100px
+          position relative
+
+          &::before
+            content ''
+            position absolute
+            top -35px
+            left 25px
+            width 70px
+            height 70px
+            background #ffffff
+            border-radius 50%
+            box-shadow 57px 0 0 19px #ffffff
+        .one
+          transform translate(-90px, -110px)
+          z-index 1000
+          box-shadow 8px 12px 18px -10px rgba(0,0,0,0.6)
+          opacity .7
+        .two
+          transform translate(-90px, -50px)
           z-index 3000
+          box-shadow 8px 12px 15px -7px rgba(0,0,0,0.4)
+          opacity .9
 </style>
